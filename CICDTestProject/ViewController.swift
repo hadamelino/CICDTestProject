9a56a5f82e4640bb10d88176473c6126fb0fe11f
hadamelino <hadamuhammad@gmail.com>
2022-09-27 14:57:14 +0700

    Initial Commit

---
+++ CICDTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTestProject
+//
+//  Created by Hada Melino Muhammad on 27/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
